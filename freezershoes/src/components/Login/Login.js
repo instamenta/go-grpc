f14760383a86264e8c677fed4798285bdff1d374
instamenta <98179343+instamenta@users.noreply.github.com>
2023-03-13 04:01:17 +0200

    css upgrade

--- freezershoes/src/components/Login/Login.js
+++ freezershoes/src/components/Login/Login.js
@@ -69,37 +69,37 @@ export const Login = () => {
     }
     return (
         <div className='main'>
-        <div className='login-container'>
-            <div className='login-decoration'>
-                
-                <h1 className='auth-h1'><i class="fa-solid fa-user-lock"></i> Login</h1>
+            <div className='login-container'>
+                <div className='login-decoration'>
 
+                    <h1 className='auth-h1'><i class="fa-solid fa-user-lock"></i> Login</h1>
+
+                </div>
+                <form
+                    // onSubmit={handleSubmit}
+                    className='form-container'>
+                    <h2 className='try-again-message'>
+                        {errors}
+                    </h2>
+                    {inputs.map((input) => (
+                        <FormInput
+                            key={input.id}
+                            {...input}
+                            value={values[input.name]}
+                            onChange={onChange}
+                        />))}
+
+                    <button className='submit-button'>
+                        Login
+                    </button>
+
+                    <Link to="/users/register" className='auth-redirect'>
+                        Redirect to register.
+                    </Link>
+                </form>
+                <div className='login-decoration-bottom'>
+                </div>
             </div>
-            <form 
-            // onSubmit={handleSubmit}
-             className='form-container'>
-                <h2 className='try-again-message'>
-                    {errors}
-                </h2>
-                { inputs.map((input) => (
-                    <FormInput
-                        key={input.id}
-                        {...input}
-                        value={values[input.name]}
-                        onChange={onChange}
-                    /> ))}
-                
-                <button className='submit-button'>
-                    Login
-                </button>
-                
-                <Link to="/users/register" className='auth-redirect'>
-                    Redirect to register.
-                </Link>
-            </form>
-            <div className='login-decoration-bottom'>
-            </div>
-        </div>
         </div>
     )
 }
